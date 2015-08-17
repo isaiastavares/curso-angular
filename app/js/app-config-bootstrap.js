6d1529e94d6c19686ce6e897595d693eb8a04abe
Isaias Tavares <isaias_t_neto@hotmail.com>
2015-08-17 19:12:51 -0300

    Criando tela utilizando angular material

--- app/js/app-config-bootstrap.js
+++ app/js/app-config-bootstrap.js
@@ -1,4 +1,4 @@
 ﻿/**
  * Created by Isaias on 12/08/2015.
  */
-angular.module('helloWorldApp',['ngMessages', 'ui.growl']);
+angular.module('helloWorldApp',['ngMessages', 'ui.growl', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.selection', 'ngMaterial']);
--- app/js/bootstrap-controller.js
+++ app/js/bootstrap-controller.js
@@ -10,12 +10,33 @@ function BootstrapController($scope, $grow1) {
     $scope.pessoa = {};
     $scope.pessoas = [];
 
+    $scope.gridOptions = {
+        data: 'pessoas',
+        columnDefs: [
+            { name : 'Nome', field : 'nome' } ,
+            { name : 'Sobrenome', field : 'sobrenome' },
+            { name : 'Email', field : 'email' },
+            { name : 'Data de Nascimento', field : 'nascimento', cellTemplate : 'app/templates/filterTemplate.html' },
+            { name : 'Sexo', field : 'sexo', },
+            { name : 'Ações', field : 'acoes', cellTemplate : 'app/templates/gridTemplate.html' },
+        ],
+        rowTemplate : 'app/templates/rowTemplate.html'
+    }
+
+    $scope.getRowStyle = function(row) {
+        var rowStyle = {};
+        if (angular.isDefined(row.entity.cor)) {
+            rowStyle.backgroundColor = row.entity.cor;
+        }
+        return rowStyle;
+    }
+
     $scope.salvar = function () {
         if ($scope.myForm.$invalid) {
-            var obj = {};
-            obj.class = 'danger';
-            obj.timeout = 4000;
-            $grow1.box("Erro", "Existem erros no formulário", obj).open();
+            $grow1.box("Atenção", "Erros encontrados no formulário.", {
+                class: 'warning',
+                timeout: 4000
+            }).open();
             return;
         }
         $scope.pessoas.push($scope.pessoa);
@@ -26,7 +47,10 @@ function BootstrapController($scope, $grow1) {
         $scope.pessoa = {};
     }
 
-    $scope.excluir = function () {
+    $scope.excluir = function (pessoa) {
+        if (pessoa) {
+            $scope.pessoa = pessoa;
+        }
         $scope.pessoas.splice($scope.pessoas.indexOf($scope.pessoa),1);
         $scope.limpar();
     }
